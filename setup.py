b150427777a6e2a1aff75623042626b8a49f2737
ErnestBeckham <ernestbeckham1998@gmail.com>
2025-07-06 20:58:34 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,2 +1,8 @@
 from setuptools import setup
-setup()
+
+setup(
+    name="tom-pacman-env",
+    version="0.1.0",
+    packages=["pac_man"],  # Only install this package!
+    install_requires=["gymnasium", "pygame", "numpy"],
+)
